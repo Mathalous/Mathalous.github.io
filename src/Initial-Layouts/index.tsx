57c89c11a0d369a22b79d8b09e60d1e6f734a452
Matthew <person.miller.matthew@gmail.com>
2022-06-29 23:57:06 -0500

    fix layout

--- src/Initial-Layouts/index.tsx
+++ src/Initial-Layouts/index.tsx
@@ -1,253 +1,286 @@
 import { Layouts } from 'react-grid-layout';
 
 export const AllLayouts : Layouts = {
-  "lg": [
-      {
-          "w": 4,
-          "h": 6,
-          "x": 0,
-          "y": 0,
-          "i": "Overview",
-          "moved": false,
-          "static": false
-      },
-      {
-          "w": 4,
-          "h": 5,
-          "x": 0,
-          "y": 6,
-          "i": "Skills",
-          "moved": false,
-          "static": false
-      },
-      {
-        "w": 4,
-        "h": 5,
-        "x": 0,
-        "y": 6,
-        "i": "Languages",
-        "moved": false,
-        "static": false
-    },
-      {
-          "w": 4,
-          "h": 5,
-          "x": 0,
-          "y": 11,
-          "i": "Projects",
-          "moved": false,
-          "static": false
-      },
-      {
-          "w": 4,
-          "h": 6,
-          "x": 0,
-          "y": 16,
-          "i": "Background",
-          "moved": false,
-          "static": false
-      },
-      {
-          "w": 20,
-          "h": 22,
-          "x": 4,
-          "y": 0,
-          "i": "Inspector",
-          "moved": false,
-          "static": false
-      }
-  ],
-  "md": [
-      {
-          "w": 2,
-          "h": 6,
-          "x": 0,
-          "y": 0,
-          "i": "Overview",
-          "moved": false,
-          "static": false
-      },
-      {
-          "w": 2,
-          "h": 6,
-          "x": 2,
-          "y": 0,
-          "i": "Skills",
-          "moved": false,
-          "static": false
-      },
-      {
-          "w": 2,
-          "h": 6,
-          "x": 8,
-          "y": 0,
-          "i": "Projects",
-          "moved": false,
-          "static": false
-      },
-      {
-          "w": 2,
-          "h": 6,
-          "x": 6,
-          "y": 0,
-          "i": "Background",
-          "moved": false,
-          "static": false
-      },
-      {
-          "w": 10,
-          "h": 7,
-          "x": 0,
-          "y": 6,
-          "i": "Inspector",
-          "moved": false,
-          "static": false
-      }
-  ],
-  "sm": [
-      {
-          "w": 4,
-          "h": 5,
-          "x": 0,
-          "y": 0,
-          "i": "Overview",
-          "moved": false,
-          "static": false
-      },
-      {
-          "w": 2,
-          "h": 5,
-          "x": 0,
-          "y": 10,
-          "i": "Skills",
-          "moved": false,
-          "static": false
-      },
-      {
-          "w": 2,
-          "h": 5,
-          "x": 0,
-          "y": 5,
-          "i": "Projects",
-          "moved": false,
-          "static": false
-      },
-      {
-          "w": 2,
-          "h": 5,
-          "x": 2,
-          "y": 5,
-          "i": "Background",
-          "moved": false,
-          "static": false
-      },
-      {
-          "w": 4,
-          "h": 15,
-          "x": 4,
-          "y": 0,
-          "i": "Inspector",
-          "moved": false,
-          "static": false
-      }
-  ],
-  "xs": [
-      {
-          "w": 2,
-          "h": 5,
-          "x": 0,
-          "y": 0,
-          "i": "Overview",
-          "moved": false,
-          "static": false
-      },
-      {
-          "w": 2,
-          "h": 5,
-          "x": 0,
-          "y": 5,
-          "i": "Skills",
-          "moved": false,
-          "static": false
-      },
-      {
-          "w": 2,
-          "h": 5,
-          "x": 2,
-          "y": 0,
-          "i": "Projects",
-          "moved": false,
-          "static": false
-      },
-      {
-          "w": 2,
-          "h": 5,
-          "x": 2,
-          "y": 5,
-          "i": "Background",
-          "moved": false,
-          "static": false
-      },
-      {
-          "w": 4,
-          "h": 9,
-          "x": 0,
-          "y": 10,
-          "i": "Inspector",
-          "moved": false,
-          "static": false
-      }
-  ],
-  "xxs": [
-      {
-          "w": 2,
-          "h": 5,
-          "x": 0,
-          "y": 0,
-          "i": "Overview",
-          "static": false
-      },
-      {
-          "w": 2,
-          "h": 5,
-          "x": 0,
-          "y": 5,
-          "i": "Skills",
-          "static": false
-      },
-      {
-          "w": 2,
-          "h": 5,
-          "x": 0,
-          "y": 10,
-          "i": "Projects",
-          "static": false
-      },
-      {
-          "w": 2,
-          "h": 7,
-          "x": 0,
-          "y": 15,
-          "i": "Careers",
-          "static": false
-      },
-      {
-          "w": 2,
-          "h": 7,
-          "x": 2,
-          "y": 15,
-          "i": "Education",
-          "static": false
-      },
-      {
-          "w": 2,
-          "h": 15,
-          "x": 2,
-          "y": 0,
-          "i": "Inspector",
-          "static": false
-      }
-  ]
+    "lg": [
+        {
+            "w": 4,
+            "h": 6,
+            "x": 0,
+            "y": 0,
+            "i": "Overview",
+            "moved": false,
+            "static": false
+        },
+        {
+            "w": 4,
+            "h": 5,
+            "x": 0,
+            "y": 6,
+            "i": "Languages",
+            "moved": false,
+            "static": false
+        },
+        {
+            "w": 4,
+            "h": 4,
+            "x": 0,
+            "y": 15,
+            "i": "Projects",
+            "moved": false,
+            "static": false
+        },
+        {
+            "w": 2,
+            "h": 4,
+            "x": 2,
+            "y": 11,
+            "i": "Background",
+            "moved": false,
+            "static": false
+        },
+        {
+            "w": 15,
+            "h": 19,
+            "x": 4,
+            "y": 0,
+            "i": "Inspector",
+            "moved": false,
+            "static": false
+        },
+        {
+            "w": 2,
+            "h": 4,
+            "x": 0,
+            "y": 11,
+            "i": "Skills",
+            "moved": false,
+            "static": false
+        }
+    ],
+    "md": [
+        {
+            "w": 2,
+            "h": 6,
+            "x": 0,
+            "y": 0,
+            "i": "Overview",
+            "moved": false,
+            "static": false
+        },
+        {
+            "w": 2,
+            "h": 7,
+            "x": 0,
+            "y": 6,
+            "i": "Languages",
+            "moved": false,
+            "static": false
+        },
+        {
+            "w": 2,
+            "h": 6,
+            "x": 2,
+            "y": 0,
+            "i": "Projects",
+            "moved": false,
+            "static": false
+        },
+        {
+            "w": 2,
+            "h": 4,
+            "x": 2,
+            "y": 9,
+            "i": "Background",
+            "moved": false,
+            "static": false
+        },
+        {
+            "w": 6,
+            "h": 13,
+            "x": 4,
+            "y": 0,
+            "i": "Inspector",
+            "moved": false,
+            "static": false
+        },
+        {
+            "w": 2,
+            "h": 3,
+            "x": 2,
+            "y": 6,
+            "i": "Skills",
+            "moved": false,
+            "static": false
+        }
+    ],
+    "sm": [
+        {
+            "w": 3,
+            "h": 5,
+            "x": 0,
+            "y": 0,
+            "i": "Overview",
+            "moved": false,
+            "static": false
+        },
+        {
+            "w": 3,
+            "h": 4,
+            "x": 0,
+            "y": 5,
+            "i": "Languages",
+            "moved": false,
+            "static": false
+        },
+        {
+            "w": 3,
+            "h": 3,
+            "x": 0,
+            "y": 13,
+            "i": "Projects",
+            "moved": false,
+            "static": false
+        },
+        {
+            "w": 2,
+            "h": 4,
+            "x": 1,
+            "y": 9,
+            "i": "Background",
+            "moved": false,
+            "static": false
+        },
+        {
+            "w": 3,
+            "h": 16,
+            "x": 3,
+            "y": 0,
+            "i": "Inspector",
+            "moved": false,
+            "static": false
+        },
+        {
+            "w": 1,
+            "h": 4,
+            "x": 0,
+            "y": 9,
+            "i": "Skills",
+            "moved": false,
+            "static": false
+        }
+    ],
+    "xs": [
+        {
+            "w": 3,
+            "h": 5,
+            "x": 0,
+            "y": 0,
+            "i": "Overview",
+            "moved": false,
+            "static": false
+        },
+        {
+            "w": 3,
+            "h": 5,
+            "x": 0,
+            "y": 5,
+            "i": "Languages",
+            "moved": false,
+            "static": false
+        },
+        {
+            "w": 2,
+            "h": 5,
+            "x": 0,
+            "y": 10,
+            "i": "Projects",
+            "moved": false,
+            "static": false
+        },
+        {
+            "w": 2,
+            "h": 5,
+            "x": 2,
+            "y": 10,
+            "i": "Background",
+            "moved": false,
+            "static": false
+        },
+        {
+            "w": 3,
+            "h": 10,
+            "x": 3,
+            "y": 0,
+            "i": "Inspector",
+            "moved": false,
+            "static": false
+        },
+        {
+            "w": 2,
+            "h": 5,
+            "x": 4,
+            "y": 10,
+            "i": "Skills",
+            "moved": false,
+            "static": false
+        }
+    ],
+    "xxs": [
+        {
+            "w": 4,
+            "h": 5,
+            "x": 0,
+            "y": 0,
+            "i": "Overview",
+            "moved": false,
+            "static": false
+        },
+        {
+            "w": 4,
+            "h": 6,
+            "x": 0,
+            "y": 13,
+            "i": "Languages",
+            "moved": false,
+            "static": false
+        },
+        {
+            "w": 4,
+            "h": 5,
+            "x": 0,
+            "y": 19,
+            "i": "Projects",
+            "moved": false,
+            "static": false
+        },
+        {
+            "w": 4,
+            "h": 4,
+            "x": 0,
+            "y": 24,
+            "i": "Background",
+            "moved": false,
+            "static": false
+        },
+        {
+            "w": 4,
+            "h": 8,
+            "x": 0,
+            "y": 5,
+            "i": "Inspector",
+            "moved": false,
+            "static": false
+        },
+        {
+            "w": 4,
+            "h": 4,
+            "x": 0,
+            "y": 28,
+            "i": "Skills",
+            "moved": false,
+            "static": false
+        }
+    ]
 }
 
 export default AllLayouts
