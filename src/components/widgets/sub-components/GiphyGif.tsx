cfc2bcd9db9a3cd273e3d8f86217b7365c1a028c
Matthew <person.miller.matthew@gmail.com>
2022-07-04 16:26:33 -0500

    adding comment

--- src/components/widgets/sub-components/GiphyGif.tsx
+++ src/components/widgets/sub-components/GiphyGif.tsx
@@ -3,9 +3,12 @@ const defaultStyle : React.CSSProperties = {
   flex:'1 1',
   flexDirection:'column'
 }
+//TODO: look into error from chrome blocking Giphy Cookies
 export function Giphy(props:{title:string, src:string, style?:React.CSSProperties}){
     return <div style={{...defaultStyle, ...props.style}}>
-      <iframe title={props.title} style={{display:'block', margin:'auto', height:'100%', width:'100%'}} src={props.src} 
-        frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
+      <iframe  title={props.title} style={{display:'block', margin:'auto', height:'100%', width:'100%'}} 
+        src={props.src} frameBorder="0" className="giphy-embed" allowFullScreen
+      
+      ></iframe>
   </div>
 }
