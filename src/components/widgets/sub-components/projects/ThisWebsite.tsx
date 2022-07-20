74dbfafe713d5f4ef655d4a4930b3dd47cb201bd
Matthew <person.miller.matthew@gmail.com>
2022-07-19 21:03:00 -0500

    updating this text

--- src/components/widgets/sub-components/projects/ThisWebsite.tsx
+++ src/components/widgets/sub-components/projects/ThisWebsite.tsx
@@ -5,13 +5,12 @@ export function ThisWebsite(props:{item:IProject}){
     <h3 style={{textAlign:'center'}} >{props.item.name}</h3>
     <p>
       This website was created using React, Typescript, and react-grid-layout.
-      All gifs are hosted using embedded Giphy.
+      All gifs are hosted using embedded Giphy. It’s intent is to showcase a few of my projects while giving me an excuse to further my frontend skills
+      To achieve this goal, I put a few artificial challenges in place. The grid itself can be adjusted and a new layout generated from the placement. 
+      Furthermore, adding a new item to the config will add the button to the relevent section, though a customized view can be added later to refine the view. 
     </p>
     <p>
-      It’s intent is to showcase a few of my projects while giving me an excuse to further my frontend skills
-    </p>
-    <p>
-      The source code is avalible on <a href="https://github.com/Mathalous/Mathalous.github.io/tree/main" >Github</a>
+      Source code is avalible on <a href="https://github.com/Mathalous/Mathalous.github.io/tree/main" >Github</a>
     </p>
   </div>
 }
